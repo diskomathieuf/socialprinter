0189b3e9e6607966c9dafac4c9d390d03e616647
mathieu-fernandez <mathieu.yvan@disko.fr>
2015-10-30 11:11:55 +0100

    send email with the picture and handle "not logged" user

--- src/index.php
+++ src/index.php
@@ -1,22 +1,35 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+date_default_timezone_set('Europe/Paris');
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Constraints as Assert;
+
 $app = new Silex\Application();
 if ($_SERVER['HTTP_HOST'] === 'socialprinter.localhost') $app['debug'] = true;
 
 $app['insta'] = new MetzWeb\Instagram\Instagram(array(
     'apiKey'      => '8fdab76ee73e41a18e8d6286021c69f6',
     'apiSecret'   => '87c023f0969f4134a7a97727a880e595',
-    'apiCallback' => 'http://socialprinter.localhost/choose-color'
+    'apiCallback' => 'http://socialprinter.localhost/insta-image'
 ));
+$app['printerMail'] = 'mathieu.yvan@disko.fr';
 
+$app->register(new Silex\Provider\SwiftmailerServiceProvider());
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 $app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\FormServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__ . '/templates',
 ));
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.domains' => array(),
+));
 
-$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+$app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
     $twig->addFunction(new \Twig_SimpleFunction('asset', function ($asset) use ($app) {
         return sprintf('%s/%s', trim($app['request']->getBasePath()), ltrim($asset, '/'));
     }));
@@ -25,9 +38,10 @@ $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
 
 // ROUTES
 $app->get('/', function (Silex\Application $app) {
+    $app['session']->clear();
     $loginUrl = $app['insta']->getLoginUrl();
 
-    return $app['twig']->render('index.html.twig',
+    return $app['twig']->render('index.twig',
         array(
             'loginUrl' => $loginUrl,
         )
@@ -35,7 +49,7 @@ $app->get('/', function (Silex\Application $app) {
 })
     ->bind('index');
 
-$app->get('/choose-color', function (Silex\Application $app) {
+$app->get('/insta-image', function (Silex\Application $app) {
     // receive OAuth code parameter
     $code = $_GET['code'];
 
@@ -83,36 +97,130 @@ $app->get('/choose-color', function (Silex\Application $app) {
     }
 
     return $app['twig']->render(
-        'choose-color.html.twig',
+        'insta-image.twig',
         array(
             'data'   => $data,
             'medias' => $medias,
         )
     );
 })
-    ->bind('choose-color');
+    ->bind('insta-image');
 
-$app->get('/choose-picto/', function (Silex\Application $app) {
+$app->get('/choose-logo', function (Silex\Application $app) {
     $dominantColors = $app['session']->get('dominant_colors');
     return $app['twig']->render(
-        'choose-picto.html.twig',
+        'choose-logo.twig',
         array(
             'dominantColors' => $dominantColors,
         )
     );
 })
-    ->bind('choose-picto');
+    ->bind('choose-logo');
 
-$app->get('/print-it/{id}', function (Silex\Application $app, $id) {
+$app->match('/print-it/{id}', function (Request $request, $id) use ($app) {
     $dominantColors = $app['session']->get('dominant_colors');
-    return $app['twig']->render(
-        'print-it.html.twig',
-        array(
-            'colors' => $dominantColors[$id],
-            'id' => $id,
-        )
-    );;
+    $sent = false;
+
+    //Could use short array [] but have used long arrays in other parts
+    $default = array(
+        'name'    => '',
+        'email'   => '',
+        'message' => '',
+    );
+
+    $form = $app['form.factory']->createBuilder('form', $default)
+        ->add('name', 'text', array(
+            'constraints' => array(new Assert\NotBlank(), new Assert\Length(array('min' => 3))),
+            'attr'        => array('class' => 'form-control', 'placeholder' => 'Votre prénom',),
+            'label'       => 'Prénom'
+        ))
+        ->add('email', 'email', array(
+            'constraints' => new Assert\Email(),
+            'attr'        => array('class' => 'form-control', 'placeholder' => 'Votre mail'),
+            'label'       => 'Mail'
+        ))
+        ->add('picture', 'hidden', array())
+        ->add('size', 'choice', array(
+            'constraints' => array(new Assert\NotNull()),
+            'attr'        => array('class' => 'form-control'),
+            'choices'     => array('s' => 'S', 'm' => 'M', 'l' => 'L'),
+            'label'       => 'Taille'
+        ))
+        ->add('validate', 'submit', array(
+            'attr'  => array('class' => 'btn btn-default'),
+            'label' => 'Valider'
+        ))
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isValid()) {
+        $data = $form->getData();
+        $pictureInfo = array(
+            time(),
+            $data['name'],
+            $data['size']
+        );
+
+        try {
+            $png = empty($dominantColors) ? __DIR__.'/images/logo-'.$id.'.png' : '/tmp/' . $id . '.png';
+            $message = \Swift_Message::newInstance('smtp.gmail.com', 465)
+                ->setSubject('Votre T-shirt est prêt !')
+                ->setFrom(array($data['email'] => $data['name']))
+                ->setTo(array($app['printerMail'], $data['email']))
+                ->setBody('Venez le récupérer directement à l\'imprimante')
+                ->attach(Swift_Attachment::fromPath($png)->setFilename(join('_', $pictureInfo) . '.png')
+                );
+
+            $app['mailer']->send($message);
+            $sent = true;
+        } catch (Exception $e) {
+            $sent = false;
+            echo 'error';
+        }
+    }
+
+    if ($sent) {
+        return $app['twig']->render(
+            'validate.twig'
+        );
+    } else {
+        $colors = isset($dominantColors[$id]) ? $dominantColors[$id] : null;
+        return $app['twig']->render(
+            'print-it.twig',
+            array(
+                'form'   => $form->createView(),
+                'sent'   => $sent,
+                'colors' => $colors,
+                'id'     => $id,
+            )
+        );
+    }
 })
     ->bind('print-it');
+
+$app->post('/save-picture', function (Silex\Application $app, Request $request) {
+    $picture = $request->request->get('picture');
+    if (!empty($picture)) {
+        // Remove the headers (data:,) part.
+        // A real application should use them according to needs such as to check image type
+        $filteredData = substr($picture, strpos($picture, ",") + 1);
+
+        // Need to decode before saving since the data we received is already base64 encoded
+        $unencodedData = base64_decode($filteredData);
+
+        //echo "unencodedData".$unencodedData;
+
+        // Save file. This example uses a hard coded filename for testing,
+        // but a real application can specify filename in POST variable
+        $id = $request->request->get('id');
+        $fp = fopen('/tmp/' . $id . '.png', 'w+');
+        $result = fwrite($fp, $unencodedData);
+        fclose($fp);
+    }
+    return $result;
+})
+    ->bind('save-picture');
+
 
 $app->run();
